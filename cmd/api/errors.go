774d5e625fda5f4ab9cc1d24b9a98beea82646c9
harphies <taofeekhorlarlaykan@gmail.com>
2022-03-30 14:18:39 +0100

    added: helper function to convert go object to json

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -1,8 +1,1 @@
 package main
-
-// func (app *application) logError(r *http.Request, err error) {
-// 	app.logger.PrintError(err, map[string]string{
-// 		"request_method": r.Method,
-// 		"request_url":    r.URL.String(),
-// 	})
-// }
--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// define the envoper
+type envelope map[string]interface{}
+
+// write JSON
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+
+	js, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	// Append new line to make it easier to view in terminal applications
+	js = append(js, '\n')
+
+	// loop through the header
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	// Add content type as application/json, then write the status code and the JSON response
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
+}
